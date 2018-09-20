d2b0877c4205e99f336271cbcca19b269bd6e86e
Justin Celeste <justinceleste@sashimi-macbook-pro.local>
2018-09-19 17:55:48 -0700

    Initial Commit

---
+++ CountDownApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountDownApp
+//
+//  Created by Justin Celeste on 9/19/18.
+//  Copyright © 2018 Justin Celeste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
