3551442f7f53a451edfc6b6ea640df59c4928ff2
Justin Celeste <justin.celeste.jc@gmail.com>
2018-11-15 17:21:17 -0800

    first commit

--- CountDownApp/ViewController.swift
+++ CountDownApp/ViewController.swift
@@ -9,12 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    
+    var timer = Timer()
+    var counter = 0.0
+    var isRunning = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        timeLabel.text = "\(counter)"
+        self.startButton.isEnabled = true
+        self.pauseButton.isEnabled = false
+    }
+    
+    @IBAction func startButton(_ sender: Any) {
+        if(isRunning) {
+            return
+        }
+        self.startButton.isEnabled = false
+        self.pauseButton.isEnabled = true
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #Selector(updateTimer), userInfo: nil, repeats: true)
+    
+        }
+    }
+    
+    @IBAction func pauseButtonDidTouch(_ sender: Any) {
+        self.startButton.isEnabled = false
+        self.pauseButton.isEnabled = true
+        
+        timer.invalidate()
+        isRunning = true
+        
+    }
+    
+    @IBAction func resetButtonDidTouch(_ sender: Any) {
+        self.resetButton.isEnabled = true
+        self.startButton.isEnabled = true
+        
+        
+    }
+    
+ 
+    @objc func updateTimer() {
+        counter += 0.1
+        timeLabel.text = String(format: "%.1f", counter)
+    
+        
     }
 
 
 }
-
