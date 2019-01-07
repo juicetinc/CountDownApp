2bab281bf32b651ae62f8372b9de074f49a1d6a0
Justin Celeste <justin.celeste.jc@gmail.com>
2019-01-06 16:07:22 -0800

    Updated UIButton animations, disable UIButtons when tapped accordingly

--- CountDownApp/ViewController.swift
+++ CountDownApp/ViewController.swift
@@ -16,9 +16,9 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var startButton: UIButton!
-    @IBOutlet weak var pauseButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var startButton: BounceButton!
+    @IBOutlet weak var pauseButton: BounceButton!
+    @IBOutlet weak var resetButton: BounceButton!
     
     
     var timer = Timer()
@@ -33,30 +33,49 @@ class ViewController: UIViewController {
         self.pauseButton.isEnabled = false
     }
     
-    @IBAction func startButtonTapped(_ sender: UIButton) {
+    @IBAction func startButtonTapped(_ sender: BounceButton) {
         if(isRunning) {
             return
         }
         self.startButton.isEnabled = false
         self.pauseButton.isEnabled = true
+        self.resetButton.isEnabled = true
+        
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
+        
+        // changes the opacity of the startButton when tapped
+        sender.flash()
     }
 
     
-    @IBAction func pauseButtonTapped(_ sender: UIButton) {
-        self.pauseButton.isEnabled = true
+    @IBAction func pauseButtonTapped(_ sender: BounceButton) {
+        if(isRunning) {
+            return
+        }
+        self.pauseButton.isEnabled = false
         self.startButton.isEnabled = true
+        self.resetButton.isEnabled = true
+            
         timer.invalidate()
         
+        // changes the opacity of the pauseButton when tapped
+        sender.flash()
         
     }
 
-    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    @IBAction func resetButtonTapped(_ sender: BounceButton) {
+        if(isRunning) {
+            return
+        }
         timer.invalidate()
         counter = 0
         timeLabel.text = "00:00:00"
         self.startButton.isEnabled = true
+        self.pauseButton.isEnabled = false
+        self.resetButton.isEnabled = false
         
+        // shakes the resetButton when tapped
+        sender.shake()
     }
 //format the time string and interval
     func timeString(time:TimeInterval) -> String {
@@ -66,6 +85,7 @@ class ViewController: UIViewController {
         let seconds = Int(time) % 60
         
         return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
+        
         
     }
     
